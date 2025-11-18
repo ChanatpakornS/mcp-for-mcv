62fb1a5177a77179f8d9c40c646eb4e6d173bab4
Phumsirii <phumsirisumativit@gmail.com>
2025-11-18 11:16:25 +0700

    route function to api endpoint

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,6 +1,7 @@
 from fastmcp import FastMCP
-from controllers.users import get_me
+from controllers.users import get_me, get_user_gradeletter
 
 
 def register(mcp: FastMCP):
     mcp.tool()(get_me)
+    mcp.tool()(get_user_gradeletter)
